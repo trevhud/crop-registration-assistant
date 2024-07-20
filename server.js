602e344f21eae5f2aa71f88c29b7c439b78de931
Trevor Hudson <trevhud@gmail.com>
2024-07-19 21:02:26 -0700

    vercel setup

--- server.js
+++ server.js
@@ -5,28 +5,27 @@ const fs = require('fs')
 
 const dev = process.env.NODE_ENV !== 'production'
 
-const hostname = '192.168.1.80'
-const port = 3006
+// const hostname = '192.168.1.80'
+// const port = 3006
 
-//const app = next({ dev })
-const app = next({ dev, hostname, port })
+const app = next({ dev })
+// const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
-// const httpsOptions = {
-//     key: fs.readFileSync('./key.pem'),
-//     cert: fs.readFileSync('./cert.pem')
-// }
+const httpsOptions = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+}
 
 app.prepare().then(() => {
-    // createServer(httpsOptions, (req, res) => {
-    createServer({}, (req, res) => {
+    createServer(httpsOptions, (req, res) => {
 
         const parsedUrl = parse(req.url, true)
         handle(req, res, parsedUrl)
 
-    }).listen(port, (err) => {
+    }).listen(port, hostname, (err) => {
         if(err) throw err
-        console.log('\x1b[35m%s\x1b[0m', 'event', `- started ssl server on https://localhost:${port}`)
+        console.log('\x1b[35m%s\x1b[0m', 'event', `- started ssl server on https://${hostname}:${port}`)
     })
 })
 
