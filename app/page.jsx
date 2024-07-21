f915313264f78b48df7c25e3207fef94c60818a1
Trevor Hudson <trevhud@gmail.com>
2024-07-21 14:12:20 -0700

    things

--- app/page.jsx
+++ app/page.jsx
@@ -16,5 +16,12 @@ export const metadata = {
 }
 
 export default function Page(props) {
-    return <MainPage {...props} />;
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+            <MainPage {...props} />;
+        </>
+    );
 }
